7caeb4ce5faec785893b99e260fcbed546e9100c
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-03-25 10:47:40 -0300

    feature: Install Apollo Studio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
-import { join } from 'path';
+import { join } from 'path'; 
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ApolloServerPlugin } from '@apollo/server';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault() as ApolloServerPlugin<any>
+      ],
     }),
     HelloWorldModule,
   ],
