10e1035b48441ca7576ab35465879d0d0166c29f
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-03-27 10:33:27 -0300

    feature: Query with args

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-    @Query(() => String) 
-    welloWorld(): string {
+    @Query(() => String, { name: 'hello', description: 'Returns a hello world message' }) 
+    helloWorld(): string {
         return 'Hello, World!';
+    }
+
+    @Query(() => Float, { name: 'randomNumber' })
+    getRandomNumber(): number {
+        return Math.random() * 100;
+    }
+
+    @Query(() => Int, { name: 'randomFromZeroTo' })
+    getRandomFromZeroTo(
+        @Args('to', { nullable: true, type: () => Int }) to: number = 10
+    ): number {
+        return Math.floor(Math.random() * to);
     }
 }
